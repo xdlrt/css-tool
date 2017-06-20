83df6fb7d1cf3f1795757ba7a009a35368402767
xdlrt <xdlrt0111@163.com>
2017-06-20 14:00:43 +0800

    use jslint to standardize code:smile:

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp   = require('gulp');
+var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('uglify',function(){
+gulp.task('uglify', function () {
     // js文件
     gulp.src('./css-tool.js')
-    .pipe(uglify())
-    .pipe(rename('./css-tool.min.js'))
-    .pipe(gulp.dest('./'));
+        .pipe(uglify())
+        .pipe(rename('./css-tool.min.js'))
+        .pipe(gulp.dest('./'));
 });
